677da7167cfeb4bdbe08e7a5d3e9e497f6259202
MOHAMMAD SHOYEB <mohdshoyeb189@gmail.com>
2025-09-26 23:49:40 +0530

    commented out some code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,7 +161,7 @@ export default function Page() {
         <ThreatBenefitsSection />
       </section>
 
-      <div className="w-full my-28 bg-gray-600" style={{ height: "1px" }} />
+      {/* <div className="w-full my-28 bg-gray-600" style={{ height: "1px" }} /> */}
 
        {/*new line*/}
       <IntelligenceSection />
